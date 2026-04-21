760b6ed7afa05d89221a4c44b5a38f5b151c6d56
oquaishi <oquaishi@gmail.com>
2026-04-21 12:12:36 -0400

    Bind to Railway PORT env variable

--- backend/run.py
+++ backend/run.py
@@ -1,3 +1,4 @@
+import os
 from app import create_app, db
 from dotenv import load_dotenv
 
@@ -6,4 +7,5 @@ load_dotenv()
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
